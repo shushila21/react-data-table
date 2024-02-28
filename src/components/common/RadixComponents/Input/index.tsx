a98a33dc8a484a645cd1b7f3c82fce191b9ac5e3
Shushila Budhathoki <shushila.budhathoki28@gmail.com>
2024-02-28 10:50:33 +0545

    fix: resolves error

--- src/components/common/RadixComponents/Input/index.tsx
+++ src/components/common/RadixComponents/Input/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { cn } from '@Utils/index';
+import * as React from "react";
+import { cn } from "../../../../utils";
 
 export interface IInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -9,21 +9,21 @@ const Input = React.forwardRef<HTMLInputElement, IInputProps>(
     return (
       <input
         type={type}
-        placeholder={placeholder || 'Search'}
+        placeholder={placeholder || "Search"}
         className={cn(
           `naxatw-flex naxatw-h-11 naxatw-border-b-2 naxatw-border-grey-300 naxatw-bg-transparent
             naxatw-px-3 naxatw-text-sm naxatw-text-grey-800 file:naxatw-font-medium placeholder:naxatw-text-grey-400
              hover:naxatw-border-primary-400 focus:naxatw-border-primary-400 focus:naxatw-bg-transparent
             focus:naxatw-outline-none disabled:naxatw-cursor-not-allowed disabled:naxatw-opacity-50`,
-          className,
+          className
         )}
         ref={ref}
         onClick={onClick}
         {...rest}
       />
     );
-  },
+  }
 );
-Input.displayName = 'Input';
+Input.displayName = "Input";
 
 export default Input;
