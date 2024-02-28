7a6ae550342a8ef7121ff24c950c91ead112c0a8
Shushila Budhathoki <shushila.budhathoki28@gmail.com>
2024-02-28 10:30:49 +0545

    fix: resolves utils cn error

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { DataTable } from "@myuser/react-data-table";
+import { DataTable } from "react-tanstack-data-table";
+
+console.log(DataTable, "DataTable");
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const columns = [
     {
       header: "S.N",
@@ -23,31 +19,10 @@ function App() {
     },
   ];
 
-  console.log(DataTable, "dt");
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {/* <DataTable columns={columns} /> */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tanstack React Datatable</h1>
+      <DataTable columns={columns} />
     </>
   );
 }
